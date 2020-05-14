6f78e67f3d8221875840b2d06aee3e92bbb90514
pfgherardini <federico.gherardini@gmail.com>
2020-05-13 17:22:32 -0700

    support for authorization tokens

--- R/query.R
+++ R/query.R
@@ -27,13 +27,14 @@ ensure_server_url <- function(server.url) {
 #' @param timeout Optional. The query timeout in milliseconds
 #' @param print.json If set to \code{TRUE}, prints the JSON version of the query
 #'   that is sent to the server (useful for debugging purposes)
+#' @param auth.token Optional string. The authuorization token to be used for this query
 #' @param ... Additional arguments passed to either \code{\link{convert_query_results}}
 #'   or either \code{\link{convert_pull_query_results}}
 #' @return Returns a \code{data.frame} or vector with the query results. In the former
 #'   case the column names are taken from the symbols that appear in the \code{find}
 #'   portion of the query
 #' @export
-do_query <- function(query, server.url = NULL, timeout = NULL, print.json = FALSE, ...) {
+do_query <- function(query, server.url = NULL, timeout = NULL, print.json = FALSE, auth.token = NULL, ...) {
     server.url <- ensure_server_url(server.url)
     qq <- query
     if(is.null(qq$query$"in"))
@@ -51,7 +52,8 @@ do_query <- function(query, server.url = NULL, timeout = NULL, print.json = FALS
     if(print.json)
         message(query.json)
 
-    response <- httr::RETRY("POST", url = server.url, body = query.json, encode = "raw")
+    response <- httr::RETRY("POST", url = server.url, body = query.json, encode = "raw",
+                            httr::add_headers(Authorization = paste("Token", auth.token)))
 
     response.content <- httr::content(response, simplifyVector = TRUE)
 
@@ -80,6 +82,9 @@ do_query <- function(query, server.url = NULL, timeout = NULL, print.json = FALS
         else
             ret <- convert_query_results(query, res.data.content$query_result, ...)
 
+    } else if (httr::status_code(response) == 401) {
+        stop(paste("Not authorized. If you have already login using wick::login, your authorization token may be expired.",
+                   "Please pass force = TRUE to wick::login to refresh your authorization token"))
     } else {
         if(!is.null(response$message))
             stop(response$message)
